5b9632ff2f2fc10bb179ae3345eb34b3871af637
LORIN <95187985+LRZIN@users.noreply.github.com>
2022-04-05 13:33:28 -0300

    Add files via upload

--- Gamer.js
+++ Gamer.js
@@ -10,24 +10,37 @@ var bola9 = document.querySelector('#bola9');
 var bola10 = document.querySelector('#bola10');
 var bola11 = document.querySelector('#bola11');
 var bola12 = document.querySelector('#bola12');
+var bola13 = document.querySelector('#bola13');
+var bola14 = document.querySelector('#bola14');
+var bola15 = document.querySelector('#bola15');
+var bola16 = document.querySelector('#bola16');
 
 
-
+var ads = document.querySelector('#ads');
 var cmd = document.querySelector('#cmd');
 var audio = document.querySelector('#audio');
+var bolas1 = document.querySelector('.bolas1');
+   
+  function offAds(){
+    ads.style.display = 'none';
+  }
                              
-    function bolaa(){bola1.style.backgroundColor = cmd.value; audio.play();};
-    function bolab(){bola2.style.backgroundColor = cmd.value; audio.play();};
-    function bolac(){bola3.style.backgroundColor = cmd.value; audio.play();};
-    function bolad(){bola4.style.backgroundColor = cmd.value; audio.play();};
-    function bolae(){bola5.style.backgroundColor = cmd.value; audio.play();};
-    function bolaf(){bola6.style.backgroundColor = cmd.value; audio.play();};
-    function bolag(){bola7.style.backgroundColor = cmd.value; audio.play();};
-    function bolah(){bola8.style.backgroundColor = cmd.value; audio.play();};
-    function bolai(){bola9.style.backgroundColor = cmd.value; audio.play();};
-    function bolaj(){bola10.style.backgroundColor = cmd.value; audio.play();};
-    function bolak(){bola11.style.backgroundColor = cmd.value; audio.play();};
-    function bolal(){bola12.style.backgroundColor = cmd.value; audio.play();};
+    function bola_z(){bola1.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zz(){bola2.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzz(){bola3.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzz(){bola4.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzz(){bola5.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzz(){bola6.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzz(){bola7.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzz(){bola8.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzz(){bola9.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzzz(){bola10.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzzzz(){bola11.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzzzzz(){bola12.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzzzzzz(){bola13.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzzzzzzz(){bola14.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzzzzzzzz(){bola15.style.backgroundColor = cmd.value; audio.play();};
+    function bola_zzzzzzzzzzzzzzzz(){bola16.style.backgroundColor = cmd.value; audio.play();};
                              
                              function limpar(){
                                bola1.style.backgroundColor = '#0000';
@@ -42,17 +55,26 @@ var audio = document.querySelector('#audio');
                                bola10.style.backgroundColor = '#0000';
                                bola11.style.backgroundColor = '#0000';
                                bola12.style.backgroundColor = '#0000';
+                               bola13.style.backgroundColor = '#0000';
+                               bola14.style.backgroundColor = '#0000';
+                               bola15.style.backgroundColor = '#0000';
+                               bola16.style.backgroundColor = '#0000';
+                               ads.style.display = 'block';
                              }
                              
-bola1.addEventListener('mouseover', bolaa);
-bola2.addEventListener('mouseover', bolab);
-bola3.addEventListener('mouseover', bolac);
-bola4.addEventListener('mouseover', bolad);
-bola5.addEventListener('mouseover', bolae);
-bola6.addEventListener('mouseover', bolaf);
-bola7.addEventListener('mouseover', bolag);
-bola8.addEventListener('mouseover', bolah);
-bola9.addEventListener('mouseover', bolai);
-bola10.addEventListener('mouseover', bolaj);
-bola11.addEventListener('mouseover', bolak);
-bola12.addEventListener('mouseover', bolal);
+bola1.addEventListener('mouseover', bola_z);
+bola2.addEventListener('mouseover', bola_zz);
+bola3.addEventListener('mouseover', bola_zzz);
+bola4.addEventListener('mouseover', bola_zzzz);
+bola5.addEventListener('mouseover', bola_zzzzz);
+bola6.addEventListener('mouseover', bola_zzzzzz);
+bola7.addEventListener('mouseover', bola_zzzzzzz);
+bola8.addEventListener('mouseover', bola_zzzzzzzz);
+bola9.addEventListener('mouseover', bola_zzzzzzzzz);
+bola10.addEventListener('mouseover', bola_zzzzzzzzzz);
+bola11.addEventListener('mouseover', bola_zzzzzzzzzzz);
+bola12.addEventListener('mouseover', bola_zzzzzzzzzzzz);
+bola13.addEventListener('mouseover', bola_zzzzzzzzzzzzz);
+bola14.addEventListener('mouseover', bola_zzzzzzzzzzzzzz);
+bola15.addEventListener('mouseover', bola_zzzzzzzzzzzzzzz);
+bola16.addEventListener('mouseover', bola_zzzzzzzzzzzzzzzz);
